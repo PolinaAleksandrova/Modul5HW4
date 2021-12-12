8cc81ef8d52567ebdc7077845e8d8d8ed194881e
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2021-12-12 21:48:39 +0200

    Cmponents added.

--- src/ResourceComponent.tsx
+++ src/ResourceComponent.tsx
@@ -1,4 +1,20 @@
 import React, { useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 
+interface IResource {
+   id: number;
+   name: string;
+   year: string;
+   color: string;
+   pantone_value: string;
+ }
+
+ interface IResourceOnPage {
+   page: number;
+   per_page: number;
+   total: number;
+   total_pages: number;
+  }
+
+
 export default ResourceComponent;
